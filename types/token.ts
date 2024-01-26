099398c5dd617a5cac1d0f004923cf967ecfc7eb
Paul Mourer <paul.mourer@onxmaps.com>
2024-01-26 13:47:01 -0700

    Type token athlete summary

--- types/token.ts
+++ types/token.ts
@@ -10,5 +10,28 @@ export type TokenResponse = {
   expires_in: number;
   refresh_token: string;
   access_token: string;
-  athlete: object;
+  athlete: SummaryAthlete;
+};
+
+export type SummaryAthlete = {
+  id: number;
+  username: null | string;
+  resource_state: 1 | 2 | 3;
+  firstname: string | null;
+  lastname: string | null;
+  bio: string | null;
+  city: string | null;
+  state: string | null;
+  country: string | null;
+  sex: "M" | "F" | null;
+  premium: boolean;
+  summit: boolean;
+  created_at: string;
+  updated_at: string;
+  badge_type_id: number;
+  weight: number;
+  profile_medium: string | null;
+  profile: string | null;
+  friend: null;
+  follower: null;
 };
